2234d8e5b7014ca3a13c26ed0ff05d82cc85c971
mumu <tnddls2ek@naver.com>
2024-11-25 02:27:01 +0900

    [Feat] AppDelegate에서 Window를 핸들링하도록 수정

--- SoongBook/Sources/Application/AppDelegate.swift
+++ SoongBook/Sources/Application/AppDelegate.swift
@@ -9,13 +9,27 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-//
-//
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        // Override point for customization after application launch.
-//        return true
-//    }
-
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        self.window?.rootViewController = UINavigationController(rootViewController: SDSShowcaseViewController())
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return false
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        return true
+    }
+    
 }
 
--- SoongBook/Sources/Application/SceneDelegate.swift
+++ SoongBook/Sources/Application/SceneDelegate.swift
@@ -7,21 +7,21 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+//    var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(windowScene: windowScene)
-        
-        let sdsShowcaseMenuVC = SDSShowcaseViewController()
-        let rootVC = UINavigationController(rootViewController: sdsShowcaseMenuVC)
-        
-        window?.rootViewController = rootVC
-        window?.makeKeyAndVisible()
-    }
-}
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        
+//        window = UIWindow(windowScene: windowScene)
+//        
+//        let sdsShowcaseMenuVC = SDSShowcaseViewController()
+//        let rootVC = UINavigationController(rootViewController: sdsShowcaseMenuVC)
+//        
+//        window?.rootViewController = rootVC
+//        window?.makeKeyAndVisible()
+//    }
+//}
 
